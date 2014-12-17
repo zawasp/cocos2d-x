a947fe762b10367231982ddefd545cbf78b0177e
Mircea Rila <mircea.rila@gmail.com>
2014-12-17 16:30:49 +0200

    update monocube template

--- templates/cpp-template-monocube/proj.android/jni/Application.mk
+++ templates/cpp-template-monocube/proj.android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
-NDK_TOOLCHAIN_VERSION=clang
+NDK_TOOLCHAIN_VERSION:=clang
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
@@ -8,9 +8,9 @@ APP_PLATFORM := android-10
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
   APP_OPTIM := debug
-  APP_ABI := armeabi x86
+  APP_ABI := armeabi-v7a
 else
   APP_CPPFLAGS += -DNDEBUG
   APP_OPTIM := release
-  APP_ABI := armeabi-v7a armeabi 
+  APP_ABI := armeabi-v7a x86
 endif
